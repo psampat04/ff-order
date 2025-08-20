3deaa7bc3b1f6fcac37b564a42e8f1e16c398dec
psampat04 <161168272+psampat04@users.noreply.github.com>
2025-08-19 21:57:56 -0400

    doha

--- order.py
+++ order.py
@@ -1,12 +1,12 @@
 import random
 import time
 
-teams = ["Parth", "Aarya", "Avie", "Vihaan", "Papa", "Jai", "Manav S", "Manav P", "Pranav", "Garb"]
+teams = ["Aarya", "Avie", "Vihaan", "Papa", "Jai", "Manav S", "Manav P", "Parth", "Pranav", "Garb"]
 
 random.shuffle(teams)
 
 print("FANTASY ORDER")
-time.sleep(3)
+#time.sleep(3)
 for i, v in enumerate(teams):
     print("Pick " + str(i + 1) + ": " + v)
-    time.sleep(3)
+    #time.sleep(3)
